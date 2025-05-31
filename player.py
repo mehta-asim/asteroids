99fc467d2f420fa51f839d8df3ebf68f4758097b
mehta-asim <asimmehtaidp@gmail.com>
2025-05-31 11:50:15 -0400

    added a shooting timer so a player can only shoot every 0.3s

--- player.py
+++ player.py
@@ -1,12 +1,13 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOT_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOT_SPEED, PLAYER_SHOOT_COOLDOWN
 from shot import Shot
 
 class Player(CircleShape):
     def __init__(self,x,y):
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
     
     def draw(self,screen):
         pygame.draw.polygon(screen,"white",self.triangle(),2)
@@ -28,6 +29,7 @@ class Player(CircleShape):
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.shoot_timer -= dt
 
         if keys[pygame.K_a] or keys[pygame.K_LEFT]:
            self.rotate(-dt)
@@ -45,6 +47,8 @@ class Player(CircleShape):
            self.shoot()
 
     def shoot(self):
-        new_shot = Shot(self.position.x, self.position.y)
-        new_shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOT_SPEED
+        if self.shoot_timer < 0:
+            new_shot = Shot(self.position.x, self.position.y)
+            new_shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOT_SPEED
+            self.shoot_timer = PLAYER_SHOOT_COOLDOWN
 
