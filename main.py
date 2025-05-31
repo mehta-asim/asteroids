0d7d65648d03657e8b54a57ac50ba4588d4e764e
mehta-asim <asimmehtaidp@gmail.com>
2025-05-31 11:35:31 -0400

    adding collision ligc between player and any asteroid

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -27,6 +28,12 @@ def main():
                 return
         
         updatable.update(dt)
+        for asteroid in asteroids:
+            if player.is_colliding(asteroid):
+                print("Game over!")
+                sys.exit()
+
+
         screen.fill("black")
         for obj in drawable:
             obj.draw(screen)
