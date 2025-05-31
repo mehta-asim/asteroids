e46ce30edaf53380d454943c0480b98299280621
mehta-asim <asimmehtaidp@gmail.com>
2025-05-31 10:53:05 -0400

    added a new constant for player radius created a new player file with player class whihc inherits from circle shape updated main file to import the new class, instantiate a new object and draw on that object

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while screen:
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    dt = 0
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
         
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
